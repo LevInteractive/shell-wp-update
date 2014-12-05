9ec1fe62ce672073f53641ea3054b2cd2247d0f6
Pete Saia <pete@lev-interactive.com>
2014-12-05 14:17:27 -0200

    added plugin updating abilities

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-This will simply update a wordpress site to the latest version.
+## Purpose
+
+This will simply update a wordpress site and all of its plugins to the latest version without needing to check the database, domain name or wp-config.php file. Good to use if you're managing sites with version control and want to check in the changes before going live.
 
 ## How to Use
 
-`cd` to this repo and then execute `./update-wp.sh ../my-website/`.
+1. Clone this project and `cd` in.
+2. Make the script executable: `chmod +x update-wp.sh`
+3. Do `./update-wp.sh ../my-site/`. Note that this path should be the root directory to the wordpress site.
+4. Checkout the updates! You will probably need to update the database which you can do by visiting `/wp-admin/upgrade.php?step=1`.
 
 By default the script will ignore:
 
 * /wp-content/
 * .* (any hidden files)
 * /wp-config.php
+* Anything in your .gitignore file.
+
--- update-wp.sh
+++ update-wp.sh
@@ -15,6 +15,12 @@ if [ ! -d "$DIR" ]; then
 	exit
 fi
 
+# Make sure this looks like a WordPress site.
+if [ ! -d "$DIR/wp-admin" ]; then
+	echo "ERROR: This doesn't seem to be a WordPress website."
+	exit
+fi
+
 # If we need the latest version, download.
 if [ ! -d "$LATEST_ROOT" ]; then
 	echo "Downloading latest wordpress version."
@@ -26,10 +32,24 @@ fi
 # Rsync the site.
 echo "Ryncing repository with new version."
 rsync \
-	-ar \
+	-aqr \
 	--stats \
 	--delete \
+	--filter=':- .gitignore' \
 	--exclude='/wp-content/' \
 	--exclude='.*' \
 	--exclude='/wp-config.php' \
-	$LATEST_ROOT/wordpress/ $DIR/
+	$LATEST_ROOT/wordpress/ \
+	$DIR/
+
+# Update the plugins.
+# This bit is an altered snippet from this script: http://b.ri.mu/files/wordpress-upgrade.sh
+echo "Upgrading plugins."
+CACHE_FILE="./.plugupdate.txt"
+touch $CACHE_FILE
+pluglist=$(find $DIR/wp-content/plugins/ -maxdepth 1 -type d | sed s@$DIR/wp-content/plugins/@@)
+for plugname in $pluglist ; do curl -s http://api.wordpress.org/plugins/info/1.0/$plugname.xml |grep download_link | cut -c40- | sed s/\].*// >>$CACHE_FILE ; done
+for file in $(cat $CACHE_FILE) ; do curl -s -o ./tmp.zip $file ;unzip -qq -o ./tmp.zip -d $DIR/wp-content/plugins/ ; rm ./tmp.zip ; done
+rm $CACHE_FILE
+
+echo "Updated. You probably need to update the database. You can do so at /wp-admin/upgrade.php?step=1"
